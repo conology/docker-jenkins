3d7ca40901b0fc835b124aa0d1bf119d14cc66df
willmtueting <47463441+willmtueting@users.noreply.github.com>
2020-01-19 12:26:26 +0100

    Create install_dev_env.cmd

---
+++ install_dev_env.cmd
@@ -0,0 +1,21 @@
+docker pull jenkinsci/blueocean
+docker pull docker:dind
+
+docker network create jenkins
+
+docker volume create jenkins-docker-certs
+docker volume create jenkins-data
+
+docker container run --name jenkins-docker --rm --detach ^
+  --privileged --network jenkins --network-alias docker ^
+  --env DOCKER_TLS_CERTDIR=/certs ^
+  --volume jenkins-docker-certs:/certs/client ^
+  --volume jenkins-data:/var/jenkins_home ^
+  docker:dind
+  
+docker container run --name jenkins-blueocean --rm --detach ^
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 ^
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 ^
+  --volume jenkins-data:/var/jenkins_home ^
+  --volume jenkins-docker-certs:/certs/client:ro ^
+  --publish 1000:8080 --publish 50000:50000 jenkinsci/blueocean
