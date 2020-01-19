c7f90d205fcd062de831dc0fe36194c2054e83ee
willmtueting <47463441+willmtueting@users.noreply.github.com>
2020-01-19 23:35:52 +0100

    Update install_dev_env.cmd

--- install_dev_env.cmd
+++ install_dev_env.cmd
@@ -6,15 +6,16 @@ docker network create jenkins
 docker volume create jenkins-docker-certs
 docker volume create jenkins-data
 
+
 (
-echo FROM docker:dind
+echo FROM jenkinsci/blueocean
 echo USER root
 echo RUN apk update
 echo RUN apk add python python-dev py-pip build-base libffi-dev openssl-dev gcc libc-dev make
 echo RUN pip install docker-compose
 ) > temp.Dockerfile
 
-docker build - < temp.Dockerfile -t jhg_dind
+docker build - < temp.Dockerfile -t jhg_jenkins
 
 del temp.Dockerfile
 
@@ -23,11 +24,11 @@ docker container run --name jenkins-docker --rm --detach ^
   --env DOCKER_TLS_CERTDIR=/certs ^
   --volume jenkins-docker-certs:/certs/client ^
   --volume jenkins-data:/var/jenkins_home ^
-  jhg_dind
+  docker:dind
   
 docker container run --name jenkins-blueocean --rm --detach ^
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 ^
-  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 ^
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=0 ^
   --volume jenkins-data:/var/jenkins_home ^
   --volume jenkins-docker-certs:/certs/client:ro ^
-  --publish 1000:8080 --publish 50000:50000 jenkinsci/blueocean
+  --publish 1000:8080 --publish 50000:50000 jhg_jenkins
